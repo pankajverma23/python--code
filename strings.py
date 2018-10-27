87e13de2693183d0759f560f977f8890a7686b2b
pankaj verma <pankajvermasm03@gmail.com>
2018-10-27 11:11:31 +0530

    string using built-in methods

---
+++ strings.py
@@ -0,0 +1,13 @@
+
+x = "my name is {} and my age is {}"
+print(x.format('pankaj',23))
+y="my name is pankaj and my age is 23"
+print(y.capitalize())
+print(y.upper())
+print(y.lower())
+print(y.count('a'))
+print(y.center(40,' '))
+print(y.split(" "))
+print(y.title())
+print(y.istitle())
+print(y.islower())
